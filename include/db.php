9cfa235c81b177124582317d8374d5e805708384
shiv-prakash742 <121678355+shiv-prakash742@users.noreply.github.com>
2023-02-03 11:07:20 +0530

    Revert "Initial commit"
    This reverts commit c6773038b86de2b307b64cbe5b6ad805ef020349.

--- include/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = 'localhost';
-    $user   = 'root';
-    $password = '';
-    $db ='user_blog';
-
-    $comm = new mysqli ($server,$user,$password,$db);
-?>
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-        if (!isset($_SESSION['userData1'])) {
-            header('location:frontend/blog/index.php');
-        } else {
-            header('location:backend/layout/dashboard.php');
-        }
-?>
